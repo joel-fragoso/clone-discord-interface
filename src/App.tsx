c2d23b56a2abb84680f999c7c65d3a9fd6d66557
Joel Fragoso <joelfragoso85@icloud.com>
2020-06-18 14:43:48 -0300

    chore: First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+
+import GlobalStyles from './styles/GlobalStyles';
+
+const App: FC = () => {
+  return (
+    <>
+      <h1>Discord</h1>
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
